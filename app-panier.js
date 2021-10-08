ca2ac3ca65706804a1e18a1fa279b5838e2034b8
GinsengWeb <solene.bonnardot@outlook.fr>
2021-10-08 14:11:35 +0200

    mise en place panier

--- app-panier.js
+++ app-panier.js
@@ -1,32 +1,52 @@
-let storage = window.localStorage.getItem("oricamStorage") //Créer notre stockage de panier
-storage = JSON.parse(localStorage.getItem("oricamStorage")) //On extrait notre json
+let storage = JSON.parse(localStorage.getItem("oricamStorage")) //On extrait notre json
+let camera
+let products = []
 
-let enStock = document.getElementById("panier")
-let products = storage.products
-for (let i = 0; i < products.length; i++) {
-  let camera = products[i]
-  console.log(products)
-  enStock.innerHTML += `
+if (!storage) {
+  let response = document.getElementById("result")
+  response.innerHTML += `
+     <div class="row text-light">
+          <h3>Votre panier est vide, allez vite dans notre boutique pour sélectionner votre article</h3>
+      </div>`
+} else {
+  let enStock = document.getElementById("panier")
+  products = storage.products
+  for (let i = 0; i < products.length; i++) {
+    camera = products[i]
+    console.log(products)
+    enStock.innerHTML += `
     <hr>
     <div class="product col-7">
       <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-6">
+        <div class="col-12">
           <h5>Nom de l'appareil</h5>
           <p class="title">${camera.name}</p>
         </div>
-        <div class="col-sm-12 col-md-6 col-lg-6">
+        <div class="col-12">
           <h4>Taille de la lentille</h4>
           <p class="lenses">${camera.lenses}</>
         </div>
       </div>
     </div>
     <div class="price col-3">
-      <p>A l'unité<br>Quantité<br>Total</p>
+      <p>A l'unité<br><br>Quantité<br><br>Total</p>
     </div>
     <div class="price col-2">
-      <div class="price">${camera.priceByItems} €</div>
-      <div class="quantity">${camera.quantity} </div>
+      <div class="price">${camera.priceByItems} €</div><br>
+      <div class="quantity">${camera.quantity}
+      <button class="fa fa-trash mx-2" onclick="test(donner l'id de la camera)"></button>
+</div><br>
       <div class="total price">${camera.price} €</div>
     </div>
       <hr>`
+  }
 }
+const clearPanier = () => {
+  localStorage.clear()
+  location.reload()
+}
+// const test = (id camera en paramètre) => {
+//   localStorage.removeItem() plutot utiliser un splice
+// puis de nouveau set item pour remettre produits acutalisé dans storage
+//   console.log("yes")
+// }
--- app-product.js
+++ app-product.js
@@ -12,71 +12,70 @@ const urlApi = "http://localhost:3000/api/cameras"
 
 //API
 const params = new URL(document.location).searchParams // Récupération paramètre de recherche
-const id = params.get("id") //Récupéraiton id
+const id = params.get("id") //Récupération id
 fetch(`${urlApi}/${id}`)
   //Promise
-  .then(async result_ => {
-    const result = await result_.json() //Donne un nom au tableau json récupéré
-    camera = result //Result deviens camera
-    //Appel des fonctions
-    lenseList()
-    cameraCard()
-  })
+  .then(
+    async result_ => {
+      const result = await result_.json() //Donne un nom au tableau json récupéré
+      camera = result //Result deviens camera
+      //Appel des fonctions
+      for (let i = 0; i < camera.lenses.length; i++) {
+        const option = document.createElement("option") //Création de la liste
+        lenses.appendChild(option) // Création du noeud (plusieurs éléments de la liste)
+        option.setAttribute("value", camera.lenses[i]) //Ajout attribut/valeur
+        option.innerHTML = camera.lenses[i] // Rédaction de la valeur
+      }
+      $cameraTitle.innerHTML += `${camera.name}`
+      $cameraImg.innerHTML += `
+        <img class=
+          "card-img-top col-12 col-lg-6 col-md-6 my-3 border border-1 border-light rounded"
+          id="image"
+          src="${camera.imageUrl}" 
+          alt="Card image cap"/>`
+      $cameraDescription.innerHTML += `${camera.description}`
+      $cameraLenses.innerHTML += `${lenses.innerHTML}`
+      let $cameraPrice = document.getElementById("camera-price")
+      let quantity = document.getElementById("quantity").value
+      $cameraPrice.innerHTML = `${(camera.price * quantity) / 100} €`
+    } // MAJ prix total
+  )
   .catch(error => {
     console.log(error)
   })
 
-//LENTILLES
-const lenseList = () => {
-  // Incrémentation des lentilles
-  for (let i = 0; i < camera.lenses.length; i++) {
-    const option = document.createElement("option") //Création de la liste
-    lenses.appendChild(option) // Création du noeud (plusieurs éléments de la liste)
-    option.setAttribute("value", camera.lenses[i]) //Ajout attribut/valeur
-    option.innerHTML = camera.lenses[i] // Rédaction de la valeur
-  }
-}
-
-//CONTENUS
-const cameraCard = () => {
-  $cameraTitle.innerHTML += `${camera.name}`
-  $cameraImg.innerHTML += `<img class="card-img-top col-12 col-lg-6 col-md-6 my-3 border border-1 border-light rounded" id="image" src="${camera.imageUrl}" alt="Card image cap"/>`
-  $cameraDescription.innerHTML += `${camera.description}`
-  $cameraLenses.innerHTML += `${lenses.innerHTML}`
-  totalPrice() // MAJ prix total
-}
-
-// CALCUL PRIX
-const totalPrice = () => {
-  let $cameraPrice = document.getElementById("camera-price")
-  let quantity = document.getElementById("quantity").value
-  $cameraPrice.innerHTML = `${(camera.price * quantity) / 100} €`
-}
-
 //LOCALSTORAGE
 // Utilisation événement onclick html
 const addProduct = () => {
-  const quantity = document.querySelector("#quantity").value //Récupère la valeur de la quantité
-  let storage = window.localStorage.getItem("oricamStorage") //Créer notre stockage de panier
+  const quantity = document.querySelector("#quantity").value
+  let storage = JSON.parse(window.localStorage.getItem("oricamStorage"))
+  let products = []
   if (!storage) {
     storage = {
       products: [],
     }
   } else {
-    storage = JSON.parse(storage) //On extrait notre json
+    console.log(storage)
+    for (let i = 0; i < camera.lenses.length; i++) {
+      let cameralense = ("value", camera.lenses[i]) //Ajout attribut/valeur
+      if (cameralense === inputGroupSelect01.value) {
+        console.log(camera)
+        let index = products.indexOf(storage)
+        products.splice(index, 1)
+        let newProducts = products.filter(cam => cam.id !== camera._id)
+        console.log(newProducts)
+      }
+    }
+    storage.products.push({
+      name: camera.name,
+      _id: camera._id,
+      lenses: inputGroupSelect01.value,
+      quantity: quantity,
+      price: (camera.price * quantity) / 100,
+      priceByItems: camera.price / 100,
+    })
   }
-  storage.products.push({
-    name: camera.name,
-    _id: camera._id,
-    lenses: inputGroupSelect01.value,
-    quantity: quantity,
-    price: camera.price * quantity,
-    priceByItems: camera.price,
-  })
+  console.log("est ajouté au storage")
   window.localStorage.setItem("oricamStorage", JSON.stringify(storage))
   console.log("localStorage", storage.products)
-  blurRemove()
-  alert(
-    `${quantity} appareil ${camera.name} lentille  ${inputGroupSelect01.value} ajouté à votre panier !`
-  )
 }
